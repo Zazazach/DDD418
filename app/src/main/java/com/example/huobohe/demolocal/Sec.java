9845fc4c56b970208fc23fa381ccec9cd450ee65
霍柏贺 <huobohe@huobohedeMacBook-Pro.local>
2017-04-18 16:24:58 +0800

    no message

---
+++ app/src/main/java/com/example/huobohe/demolocal/Sec.java
@@ -0,0 +1,27 @@
+package com.example.huobohe.demolocal;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * .       _ooOoo_
+ * .      o8888888o
+ * .      88" . "88
+ * .      (| -_- |)
+ * .      O\  =  /O
+ * .   ____/`---'\____
+ * . .'  \\|     |//  `.
+ * ./  \\||| 卍  |||//  \
+ * <p>
+ * Created by Zach on 2017/4/18.
+ */
+
+public class Sec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
